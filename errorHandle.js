6b201e9e56babe3c6f104d8bf28bdb6a37f7116c
DESKTOP-LKD0OF1\user <twn005@gsd.net.tw>
2024-03-20 10:56:57 +0800

    feat: init

---
+++ errorHandle.js
@@ -0,0 +1,16 @@
+function errorHandle(res){
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    };
+    res.writeHead(400, headers);
+    res.write(JSON.stringify({
+        "status":"false",
+        "message": "資料錯誤"
+    }));
+    res.end();
+}
+
+module.exports = errorHandle;
